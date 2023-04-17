000e51c5612d801ad80c8e02d283696de46b65a4
Davi Rocha Macedo Sousa <davirmsousa@hotmail.com>
2023-04-17 14:36:47 -0300

    alterações gerais no produtor (tópico via argumento, reinicio de paginação e log via socket)

--- git-repos-producer/src/index.js
+++ git-repos-producer/src/index.js
@@ -1,6 +1,7 @@
 import { Kafka, CompressionTypes } from "kafkajs";
 import { Octokit } from '@octokit/rest';
 import { config } from 'dotenv';
+import { io } from 'socket.io-client';
 
 // realiza a leitura das variaveis de ambiente
 config();
@@ -19,35 +20,53 @@ const producer = kafka.producer();
 producer.connect();
 
 async function run () {
+  let kafkaTopic = process.argv[2] || process.env.KAFKA_TOPIC;
+  let serviceId = `producer-${kafkaTopic}`;
   let currentPage = 1;
+
+  const socket = io("http://localhost:3000", {
+    extraHeaders: {
+      conntype: "producer",
+      clientId: serviceId
+    }
+  });
   
   let intervalId = setInterval(async () => {
     // docs:  https://docs.github.com/pt/rest/search?apiVersion=2022-11-28#search-repositories
     //        https://docs.github.com/pt/rest/search?apiVersion=2022-11-28
     
-    octokit.search.repos({ q: `language:${process.env.KAFKA_TOPIC}`, per_page: 100, page: currentPage })
+    octokit.search.repos({ q: `language:${kafkaTopic}`, per_page: 10, page: currentPage })
       .then(({ data }) => {
         data.items.forEach(async item => {
           await producer.send({
-            topic: process.env.KAFKA_TOPIC,
+            topic: kafkaTopic,
             compression: CompressionTypes.GZIP,
             messages: [ { value: item.name }],
           });
         });
+
+        let msg = `${serviceId} ${data.items.length} itens enviados para ${kafkaTopic}`;
+        socket.emit('message', msg);
+        console.log(msg);
       })
       .catch(async error => {
+        socket.emit('message', `${serviceId} apresentou um erro`);
         console.error(error);
+
         await producer.disconnect();
         clearInterval(intervalId);
       });
 
-      if (currentPage == 10) {
-        clearInterval(intervalId);
+      if (currentPage == 100) {
+        let msg = `${serviceId} limite de requisições atingido. Reiniciando.`;
+        socket.emit('message', msg);
+        console.log(msg);
+        currentPage = 1;
         return;
       }
 
       currentPage++;
-  }, 5000);
-}
+  }, 7000);
+};
 
 run().catch(console.error);
